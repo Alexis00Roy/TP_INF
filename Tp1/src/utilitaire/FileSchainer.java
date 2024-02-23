9c53b5e96c5b497612cdd832b90f637cc4979f42
Alexis00Roy <156560114+Alexis00Roy@users.noreply.github.com>
2024-02-23 14:13:15 -0500

    fesgrdtfh

---
+++ Tp1/src/utilitaire/FileSchainer.java
@@ -0,0 +1,62 @@
+package utilitaire;
+
+public class FileSchainer {
+	private int nbElement;
+	private Node premier;
+	private Node dernier;
+	
+	public FileSchainer() {
+		nbElement=0;
+		this.premier= null;
+		this.dernier= null;
+	}
+	
+	
+	public void push(Object data) {
+		if(nbElement==0) {
+			dernier = new Node(data,null);
+			premier = dernier;
+		}
+		else {
+			dernier.next= new Node(data,null );
+			dernier=dernier.next;
+		}
+		nbElement++;
+	}
+	
+	public Object pop(){
+		Object result=premier.data;
+		premier.data= null;
+		premier=premier.next;
+		return result;
+		
+	}
+	
+	public boolean estVide() {
+		boolean estVide;
+		if(nbElement==0) {
+			estVide=true;
+		}
+		else
+			estVide=true;
+		return estVide;
+	}
+	
+	
+	
+	
+	private class Node{
+		private Object data;
+		private Node next;
+		
+		public Node(Object data, Node next) {
+			this.data=data;
+			this.next = next;
+		}
+		
+		public Node(Object data) {
+			this.data=data;
+			this.next = null;
+		}
+	}
+}
---
+++ Tp1/src/utilitaire/Vect2D.java
@@ -0,0 +1,57 @@
+package utilitaire;
+import java.lang.Math;
+public class Vect2D {
+	private double Lx;
+	private double Ly;
+	
+	public Vect2D() {
+		Lx=1;
+		Ly=1;
+	}
+	public Vect2D(double x, double y) {
+		Lx=x;
+		Ly=y;
+	}
+	
+	public double getLy() {
+		return Lx;
+	}
+	
+	public double getLx() {
+		return Ly;
+	}
+	
+	public double Longueur() {
+		return Math.sqrt((Lx*Lx)+(Ly*Ly));
+	}
+	
+	public double getAngle() {
+		return Math.atan2(Ly, Lx);
+	}
+	
+	 public Vect2D calculerDiff(Vect2D posFin) {
+		 Vect2D Diff= new Vect2D(posFin.getLx()-Lx,posFin.getLy()-Ly);
+		 return Diff;
+	 }
+	 
+	 public void diviser(double div) {
+		 Lx=Lx/div;
+		 Ly=Ly/div;
+	 }
+	 
+	 public void ajouter(double x,double y) {
+		 Lx=Lx +x;
+		 Ly= Ly+y;
+	 }
+	 
+	 public boolean equals(Vect2D v) {
+		 boolean equal= false;
+		 if((v.getLx()==Lx)&&(v.getLy()==Ly))
+			 equal=true;
+		 
+		 return equal;
+	 }
+	 
+
+}
+//ghjwergtfojehwbenfrgdjkejwhbrf
